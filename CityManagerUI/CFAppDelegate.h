43a76a059827155d08525063f7536718417679f4
颜风 <1178328901@qq.com>
2014-06-18 08:27:32 +0800

    Initial Commit

---
+++ CityManagerUI/CFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CFAppDelegate.h
+//  CityManagerUI
+//
+//  Created by   颜风 on 14-6-18.
+//  Copyright (c) 2014年 Shadow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CityManagerUI/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CityManagerUI
+//
+//  Created by   颜风 on 14-6-18.
+//  Copyright (c) 2014年 Shadow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CFAppDelegate class]));
+    }
+}
