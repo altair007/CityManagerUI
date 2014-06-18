18ec4cf348d3a736d75ce3b4aed62d37a4102695
颜风 <1178328901@qq.com>
2014-06-18 18:07:20 +0800

    使用使用KVC语法替代多重遍历.部分成功.

--- CityManagerUI/core/CFCityModel.m
+++ CityManagerUI/core/CFCityModel.m
@@ -8,6 +8,36 @@
 
 #import "CFCityModel.h"
 
+
+/*一个测试类*/
+@interface City : NSObject
+@property (retain, nonatomic) NSString * name;//!< 城市名称.
+@property (retain, nonatomic) NSArray * area; //!< 城市行政区域.
+@property (retain, nonatomic) City * city;
+- (instancetype) initWithName: (NSString *) name
+                     withArea: (NSArray *) area;
+@end
+@implementation City
+-(void)dealloc
+{
+    self.name = nil;
+    self.area = nil;
+    
+    [super dealloc];
+}
+- (instancetype) initWithName: (NSString *) name
+                     withArea: (NSArray *) area
+{
+    if (self = [super init]) {
+        self.name = name;
+        self.area = area;
+        self.city = self;
+    }
+    
+    return self;
+}
+@end
+
 @interface CFCityModel ()
 @property (retain, nonatomic, readwrite) NSArray * originalData; //!< 原始数据.
 @end
@@ -40,74 +70,82 @@
 {
     NSMutableArray * result = [[NSMutableArray alloc] initWithCapacity:self.originalData.count];
     
-    [self.originalData enumerateObjectsUsingBlock:^(NSDictionary  * obj, NSUInteger idx, BOOL *stop) {
-        // 获取省名.
-        NSString * name = [obj objectForKey: @"name"];
-        [result addObject: name];
-    }];
+    result = [self.originalData valueForKey: @"name"];
     
     return result;
 }
 
 - (NSArray *) infoOfProvince: (NSString *) province;
 {
-    NSMutableArray * result = [[NSMutableArray alloc] initWithCapacity: 42];
+    NSArray * result;
     
-    [self.originalData enumerateObjectsUsingBlock:^(NSDictionary  * infoOfProvince, NSUInteger idx, BOOL *stop) {
-        // 获取省名.
-        NSString * nameOfProvince = [infoOfProvince objectForKey: @"name"];
-        
-        if ([nameOfProvince isEqualToString: province]) { // 找到省.
-            // 获取城市信息.
-            NSArray * infoOfCities = [infoOfProvince objectForKey: @"info"];
-            
-            // 遍历获取所有城市名称.
-            [infoOfCities enumerateObjectsUsingBlock:^(NSDictionary * infoOfCity, NSUInteger idx, BOOL *stop) {
-                // 获取城市名称
-                NSString * nameOfCity = [infoOfCity objectForKey: @"name"];
-                [result addObject: nameOfCity];
-            }];
-            
-            *stop = YES;
-        }
-    }];
+    result = [self.originalData filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"name == %@", province]];
+    
+    if (0 == result.count) {
+        return nil;
+    }
+    
+    result = [result objectAtIndex: 0];
+    
+    result = [result valueForKeyPath:@"info.name"];
     
     return result;
 }
 
 - (NSArray *) infoOfCith: (NSString *) city
 {
-    NSMutableArray * result = [[NSMutableArray alloc] initWithCapacity: 42];
+    NSArray * result;
     
-    // ???:三重遍历,总觉得可以简化下!要么是重组数据源,要么是改变遍历搜索算法.
-    // ???:使用KVC简化语法.
-    [self.originalData enumerateObjectsUsingBlock:^(NSDictionary  * infoOfProvince, NSUInteger idx, BOOL *stopFirst) {
-        // 获取各市信息.
-        NSArray * infoOfCities = [infoOfProvince objectForKey: @"info"];
-        [infoOfCities enumerateObjectsUsingBlock:^(NSDictionary * infoOfCity, NSUInteger idx, BOOL *stopSencond) {
-            // 获取城市名称.
-            NSString * nameOfCity = [infoOfCity objectForKey: @"name"];
-            
-            if ([nameOfCity isEqualToString: city]) { // 找到目标.
-                // 获取区县信息.
-                NSArray * infoOfAreas = [infoOfCity objectForKey: @"info"];
-                
-                // 遍历获取所有城市名称.
-                [infoOfAreas enumerateObjectsUsingBlock:^(NSDictionary * area, NSUInteger idx, BOOL *stopThird) {
-                    // 获取区县名称.
-                    NSString * nameOfArea = [area objectForKey: @"name"];
-                    
-                    [result addObject: nameOfArea];
-                }];
-                
-                * stopFirst = YES;
-                * stopSencond = YES;
-            }
-            
-        }];
-    }];
+    result = [self.originalData valueForKeyPath:@"info.info"];
+    
+    //???:  必须用block结合谓词过滤了! 总觉得可以通过某种方式简化!
+    result = [result filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"name == %@", city]];
+    
+    if (0 == result.count) {
+        return nil;
+    }
+    
+    result = [result objectAtIndex: 0];
+
+    
+    result = [result valueForKeyPath:@"info.name"];
     
     return result;
+    
+    
+//    NSMutableArray * result = [[NSMutableArray alloc] initWithCapacity: 42];
+//    
+//    // ???:使用一个类存储城市信息:国家:省:市.
+//    
+//    // ???:三重遍历,总觉得可以简化下!要么是重组数据源,要么是改变遍历搜索算法.
+//    // ???:使用KVC简化语法.
+//    [self.originalData enumerateObjectsUsingBlock:^(NSDictionary  * infoOfProvince, NSUInteger idx, BOOL *stopFirst) {
+//        // 获取各市信息.
+//        NSArray * infoOfCities = [infoOfProvince objectForKey: @"info"];
+//        [infoOfCities enumerateObjectsUsingBlock:^(NSDictionary * infoOfCity, NSUInteger idx, BOOL *stopSencond) {
+//            // 获取城市名称.
+//            NSString * nameOfCity = [infoOfCity objectForKey: @"name"];
+//            
+//            if ([nameOfCity isEqualToString: city]) { // 找到目标.
+//                // 获取区县信息.
+//                NSArray * infoOfAreas = [infoOfCity objectForKey: @"info"];
+//                
+//                // 遍历获取所有城市名称.
+//                [infoOfAreas enumerateObjectsUsingBlock:^(NSDictionary * area, NSUInteger idx, BOOL *stopThird) {
+//                    // 获取区县名称.
+//                    NSString * nameOfArea = [area objectForKey: @"name"];
+//                    
+//                    [result addObject: nameOfArea];
+//                }];
+//                
+//                * stopFirst = YES;
+//                * stopSencond = YES;
+//            }
+//            
+//        }];
+//    }];
+//    
+//    return result;
 }
 
 @end
